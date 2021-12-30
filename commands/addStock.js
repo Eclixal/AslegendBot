1ff3aea4bb590f9a1056826884f3b4aa7c53f34c
Eclixal <28450187+eclixal@users.noreply.github.com>
2021-12-30 20:08:36 +0100

    update

--- commands/addStock.js
+++ commands/addStock.js
@@ -10,15 +10,7 @@ module.exports = {
             option.setName('stockage')
                 .setDescription('Le type de stockage a modifier')
                 .setRequired(true)
-                .addChoice('Roue de secours', 'Roue de secours')
-                .addChoice('Trousse à outils', 'Trousse à outils')
-                .addChoice('Bidon d\'essence', 'Bidon d\'essence')
-                .addChoice('Moteur', 'Moteur')
-                .addChoice('Bombe de peinture', 'Bombe de peinture')
-                .addChoice('Liqueur de Stricker', 'Liqueur de Stricker')
-                .addChoice('Boisson', 'Boisson')
-                .addChoice('Nourriture', 'Nourriture')
-                .addChoice('Trousse de soin', 'Trousse de soin'))
+                .addChoice('Kit de lavage', 'Kit de lavage'))
         .addIntegerOption(option => option.setName('stock').setRequired(true).setDescription('Entrer un nombre a ajouter')),
     async execute(interaction) {
         let reply = await interaction.reply({ content: 'Ajout dans le stock avec succès !', ephemeral: false });
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {Routes} = require("discord-api-types/v9");
 const {REST} = require("@discordjs/rest");
 const rest = new REST({ version: '9' }).setToken(process.env.CLIENT_TOKEN);
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -17,66 +17,53 @@ for (const file of commandFiles) {
 
 const perms = [
     {
-        id: '881464315234885653',
+        id: '910281415017521196',
         type: 'ROLE',
         permission: true,
     },
     {
-        id: '876103493751029802',
+        id: '910281415017521194',
         type: 'ROLE',
         permission: true,
     },
+    {
+        id: '910281415017521193',
+        type: 'ROLE',
+        permission: true,
+    },
+    {
+        id: '910281415017521197',
+        type: 'ROLE',
+        permission: true,
+    },
+    {
+        id: '910281415017521199',
+        type: 'ROLE',
+        permission: true,
+    }
 ];
 
 client.on('ready', async () => {
     client.user.setActivity('Travail', { type: 'COMPETING' });
-
+    //
     // let t = await rest.get(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID));
     // for (const tt of t) {
     //     const command = await client.guilds.cache.get(process.env.GUILD_ID)?.commands.fetch(tt.id);
-    //     await command.permissions.set({ perms });
+    //     await command.permissions.set({ permissions: perms, command: 926187070207172651 });
     // }
 
-    // const stock = new MessageEmbed()
-    //     .setColor('#bbff00')
-    //     .setTitle('Gestion des stocks')
-    //     .addFields(
-    //         {
-    //             "name": `Roue de secours`,
-    //             "value": `0`
-    //         },
-    //         {
-    //             "name": `Trousse à outils`,
-    //             "value": `0`
-    //         },
-    //         {
-    //             "name": `Bidon d'essence`,
-    //             "value": `0`
-    //         },
-    //         {
-    //             "name": `Moteur`,
-    //             "value": `0`
-    //         },
-    //         {
-    //             "name": `Bombe de peinture`,
-    //             "value": `0`
-    //         },
-    //         {
-    //             "name": `Liqueur de Stricker`,
-    //             "value": `0`
-    //         },
-    //         {
-    //             "name": `Boisson`,
-    //             "value": `0`
-    //         },
-    //         {
-    //             "name": `Nourriture`,
-    //             "value": `0`
-    //         }
-    //     )
-    //     .setTimestamp()
+    const stock = new MessageEmbed()
+        .setColor('#bbff00')
+        .setTitle('Gestion des stocks')
+        .addFields(
+            {
+                "name": `Kit de lavage`,
+                "value": `0`
+            }
+        )
+        .setTimestamp()
 
-    const channel = client.channels.cache.get('876110522037194812');
+    const channel = client.channels.cache.get('925755087320678460');
     // channel.send({ embeds: [stock] });
 
     // Open / close
@@ -96,72 +83,72 @@ client.on('ready', async () => {
     // openCloseChannel.send({ embeds: [open] });
     // openCloseChannel.send({ embeds: [close] });
 
-    let guild = await client.guilds.fetch(process.env.GUILD_ID);
-    let role = await guild.roles.fetch(process.env.ROLE);
-
-    if (role.members.size >= 1) {
-        const embedMessage = await client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946');
-        const embed = embedMessage.embeds[0];
-        if (embed.title === 'Le mécano est fermé !') {
-            embed.setTimestamp()
-            embed.setColor("#0aff00")
-            embed.setTitle("Le mécano est ouvert !")
-            embed.setImage("https://media.discordapp.net/attachments/876183553564606494/881841874145525811/Paleto_Repair_Ouvert1.jpg")
-            client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946').then(message => message.edit({ embeds: [embed] }))
-            // client.channels.cache.get('876127166490886245').send('<@&876103497643327498>').then(msg => {
-            //     setTimeout(() => msg.delete(), 500)
-            // });
-        }
-    } else if (role.members.size === 0) {
-        const embedMessage = await client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946');
-        const embed = embedMessage.embeds[0];
-        if (embed.title === 'Le mécano est ouvert !') {
-            embed.setTimestamp()
-            embed.setColor("#ff0000")
-            embed.setTitle("Le mécano est fermé !")
-            embed.setImage("https://cdn.discordapp.com/attachments/876183553564606494/881841865320726548/Paleto_Repair_Ouvert.jpg")
-            client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946').then(message => message.edit({ embeds: [embed] }))
-            // client.channels.cache.get('876127166490886245').send('<@&876103497643327498>').then(msg => {
-            //     setTimeout(() => msg.delete(), 500)
-            // });
-        }
-    }
+    // let guild = await client.guilds.fetch(process.env.GUILD_ID);
+    // let role = await guild.roles.fetch(process.env.ROLE);
+    //
+    // if (role.members.size >= 1) {
+    //     const embedMessage = await client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946');
+    //     const embed = embedMessage.embeds[0];
+    //     if (embed.title === 'Le mécano est fermé !') {
+    //         embed.setTimestamp()
+    //         embed.setColor("#0aff00")
+    //         embed.setTitle("Le mécano est ouvert !")
+    //         embed.setImage("https://media.discordapp.net/attachments/876183553564606494/881841874145525811/Paleto_Repair_Ouvert1.jpg")
+    //         client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946').then(message => message.edit({ embeds: [embed] }))
+    //         // client.channels.cache.get('876127166490886245').send('<@&876103497643327498>').then(msg => {
+    //         //     setTimeout(() => msg.delete(), 500)
+    //         // });
+    //     }
+    // } else if (role.members.size === 0) {
+    //     const embedMessage = await client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946');
+    //     const embed = embedMessage.embeds[0];
+    //     if (embed.title === 'Le mécano est ouvert !') {
+    //         embed.setTimestamp()
+    //         embed.setColor("#ff0000")
+    //         embed.setTitle("Le mécano est fermé !")
+    //         embed.setImage("https://cdn.discordapp.com/attachments/876183553564606494/881841865320726548/Paleto_Repair_Ouvert.jpg")
+    //         client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946').then(message => message.edit({ embeds: [embed] }))
+    //         // client.channels.cache.get('876127166490886245').send('<@&876103497643327498>').then(msg => {
+    //         //     setTimeout(() => msg.delete(), 500)
+    //         // });
+    //     }
+    // }
 
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('guildMemberUpdate', async (oldMember, newMember) => {
-    let guild = await client.guilds.fetch(process.env.GUILD_ID);
-    let role = await guild.roles.fetch(process.env.ROLE);
-    if (role.members.size >= 1) {
-        const embedMessage = await client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946');
-        const embed = embedMessage.embeds[0];
-        if (embed.title === 'Le mécano est fermé !') {
-            embed.setTimestamp()
-            embed.setColor("#0aff00")
-            embed.setTitle("Le mécano est ouvert !")
-            embed.setImage("https://media.discordapp.net/attachments/876183553564606494/881841874145525811/Paleto_Repair_Ouvert1.jpg")
-            client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946').then(message => message.edit({ embeds: [embed] }))
-            // client.channels.cache.get('876127166490886245').send('<@&876103497643327498>').then(msg => {
-            //     setTimeout(() => msg.delete(), 500)
-            // });
-        }
-    } else if (role.members.size === 0) {
-        const embedMessage = await client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946');
-        const embed = embedMessage.embeds[0];
-        if (embed.title === 'Le mécano est ouvert !') {
-            embed.setTimestamp()
-            embed.setColor("#ff0000")
-            embed.setTitle("Le mécano est fermé !")
-            embed.setImage("https://cdn.discordapp.com/attachments/876183553564606494/881841865320726548/Paleto_Repair_Ouvert.jpg")
-            client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946').then(message => message.edit({ embeds: [embed] }))
-            // client.channels.cache.get('876127166490886245').send('<@&876103497643327498>').then(msg => {
-            //     setTimeout(() => msg.delete(), 500)
-            // });
-        }
-    }
-
-})
+// client.on('guildMemberUpdate', async (oldMember, newMember) => {
+//     let guild = await client.guilds.fetch(process.env.GUILD_ID);
+//     let role = await guild.roles.fetch(process.env.ROLE);
+//     if (role.members.size >= 1) {
+//         const embedMessage = await client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946');
+//         const embed = embedMessage.embeds[0];
+//         if (embed.title === 'Le mécano est fermé !') {
+//             embed.setTimestamp()
+//             embed.setColor("#0aff00")
+//             embed.setTitle("Le mécano est ouvert !")
+//             embed.setImage("https://media.discordapp.net/attachments/876183553564606494/881841874145525811/Paleto_Repair_Ouvert1.jpg")
+//             client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946').then(message => message.edit({ embeds: [embed] }))
+//             // client.channels.cache.get('876127166490886245').send('<@&876103497643327498>').then(msg => {
+//             //     setTimeout(() => msg.delete(), 500)
+//             // });
+//         }
+//     } else if (role.members.size === 0) {
+//         const embedMessage = await client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946');
+//         const embed = embedMessage.embeds[0];
+//         if (embed.title === 'Le mécano est ouvert !') {
+//             embed.setTimestamp()
+//             embed.setColor("#ff0000")
+//             embed.setTitle("Le mécano est fermé !")
+//             embed.setImage("https://cdn.discordapp.com/attachments/876183553564606494/881841865320726548/Paleto_Repair_Ouvert.jpg")
+//             client.channels.cache.get('876127166490886245').messages.fetch('891335724937379946').then(message => message.edit({ embeds: [embed] }))
+//             // client.channels.cache.get('876127166490886245').send('<@&876103497643327498>').then(msg => {
+//             //     setTimeout(() => msg.delete(), 500)
+//             // });
+//         }
+//     }
+//
+// })
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
@@ -172,21 +159,19 @@ client.on('interactionCreate', async interaction => {
 
     try {
         if (interaction.commandName === 'add-stock') {
-            const embedMessage = await client.channels.cache.get('876110522037194812').messages.fetch(process.env.MESSAGE);
+            const embedMessage = await client.channels.cache.get('925755087320678460').messages.fetch(process.env.MESSAGE);
             const embed = embedMessage.embeds[0];
             embed.setTimestamp()
             let pos = (embed.fields.findIndex(t => t.name === interaction.options.getString('stockage')))
             embed.fields[pos] = { name: interaction.options.getString('stockage'), value: (parseInt(embed.fields[pos].value) + parseInt(interaction.options.getInteger('stock'))).toString()}
-            client.channels.cache.get('876110522037194812').messages.fetch(process.env.MESSAGE).then(message => message.edit({ embeds: [embed] }))
-            client.channels.cache.get("888402115540680716").send(interaction.user.username + ' > add-stock type:' + interaction.options.getString('stockage') + " nb + :" + (parseInt(interaction.options.getInteger('stock'))) + ' - total : ' + (parseInt(embed.fields[pos].value) + parseInt(interaction.options.getInteger('stock'))));
+            client.channels.cache.get('925755087320678460').messages.fetch(process.env.MESSAGE).then(message => message.edit({ embeds: [embed] }))
         } else if (interaction.commandName === 'remove-stock') {
-            const embedMessage = await client.channels.cache.get('876110522037194812').messages.fetch(process.env.MESSAGE);
+            const embedMessage = await client.channels.cache.get('925755087320678460').messages.fetch(process.env.MESSAGE);
             const embed = embedMessage.embeds[0];
             embed.setTimestamp()
             let pos = (embed.fields.findIndex(t => t.name === interaction.options.getString('stockage')))
             embed.fields[pos] = { name: interaction.options.getString('stockage'), value: (parseInt(embed.fields[pos].value) - parseInt(interaction.options.getInteger('stock'))).toString()}
-            client.channels.cache.get('876110522037194812').messages.fetch(process.env.MESSAGE).then(message => message.edit({ embeds: [embed] }))
-            client.channels.cache.get("888402115540680716").send(interaction.user.username + ' > remove-stock type: ' + interaction.options.getString('stockage') + " nb + : " + (parseInt(interaction.options.getInteger('stock'))) + ' - total : ' + (parseInt(embed.fields[pos].value) + parseInt(interaction.options.getInteger('stock'))));
+            client.channels.cache.get('925755087320678460').messages.fetch(process.env.MESSAGE).then(message => message.edit({ embeds: [embed] }))
         }
         await command.execute(interaction);
     } catch (error) {
